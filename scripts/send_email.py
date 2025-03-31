d2dc5c58b0867e98261c5c9edaf341c957636b20
1godfrey <mwalupindim@gmail.com>
2025-03-31 17:20:20 -0500

    chore: update smtp credentials

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -3,7 +3,7 @@ import os
 from email.message import EmailMessage
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_PORT = os.getenv("SMTP_PORT", 587)
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
